145753f0b033779ab3597491b1f8535ed5acd9d3
Amin Amos <aminamosahs@gmail.com>
2019-06-19 15:40:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,12 +57,10 @@ end
 def runner
   welcome
   initial_round
-  hit?(initial_round)
   total = display_card_total(initial_round)
   
-  until total == 21
+  until total >= 21
     hit?(total)
-    display_card_total(total)
   end
 end
     
